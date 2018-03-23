206fdc134b22fa8125a359f0db9b8e8c115b2517
zhangyc <zhangyc@zhangycs-iMac.local>
2018-03-23 16:44:59 +0800

    add Local

---
+++ Local/1000_A+B.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main (void)
+{
+	// int a[65534],b[65534], sum[65534], ch;
+	// int i = 1;
+	// scanf("%d %d", &a[0], &b[0]);
+	// sum[0] = a[0] + b[0];
+	// while((ch = getchar()) != EOF)
+	// {
+	// 	scanf("%d %d", &a[i], &b[i]);
+	// 	sum[i] = a[i] + b[i];
+	// 	i++;
+	// }
+	// for (int j = 0; j < i; j++)
+	// {
+	// 	printf("%d\n",sum[j] );
+	// }
+
+	int a,b;
+	while(scanf("%d %d", &a, &b))
+	{
+		printf("%d\n", a + b );
+	}
+	
+	return 0;
+}
